4121f10dc2e8da1c47ba9785895539beee79f887
mehulpatel99 <sunilji9929@gmail.com>
2025-04-06 19:52:54 +0530

    create release build

--- lib/demo_freelance/unity_demo.dart
+++ lib/demo_freelance/unity_demo.dart
@@ -1,39 +1,39 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_unity_widget/flutter_unity_widget.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: UnityScreen(),
-    );
-  }
-}
-
-class UnityScreen extends StatefulWidget {
-  @override
-  _UnityScreenState createState() => _UnityScreenState();
-}
-
-class _UnityScreenState extends State<UnityScreen> {
-  UnityWidgetController? _unityWidgetController;
-
-  void onUnityCreated(UnityWidgetController controller) {
-    _unityWidgetController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter Unity Integration")),
-      body: UnityWidget(
-        onUnityCreated: onUnityCreated,
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: UnityScreen(),
+//     );
+//   }
+// }
+//
+// class UnityScreen extends StatefulWidget {
+//   @override
+//   _UnityScreenState createState() => _UnityScreenState();
+// }
+//
+// class _UnityScreenState extends State<UnityScreen> {
+//   UnityWidgetController? _unityWidgetController;
+//
+//   void onUnityCreated(UnityWidgetController controller) {
+//     _unityWidgetController = controller;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Flutter Unity Integration")),
+//       body: UnityWidget(
+//         onUnityCreated: onUnityCreated,
+//       ),
+//     );
+//   }
+// }
